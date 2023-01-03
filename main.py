8074bd8af707120570b112ac6c5d2207bbc59dd4
gintoki420 <jirichard2012@gmail.com>
2023-01-04 10:54:13 +1100

    different message

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ bot = commands.Bot(command_prefix = "e!", intents = intents)
 
 load_dotenv()
 
-
 @client.event
 async def on_ready():
     await client.change_presence(status = discord.Status.online, activity = discord.Activity(type = discord.ActivityType.watching, name = 'chill'))
@@ -19,7 +18,7 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     if member.guild.name == 'red・velvet ♡':  #type your server name
-        embed = discord.Embed(title=f'', description = f'welcome to {member.guild.name} {member.mention} \n ♥ [roles](https://discord.gg/mUcFFgQv2X)﹒[ping](https://discord.gg/xWADmpcGMQ)﹒[chat](https://discord.gg/PH7zb2QPUK)﹒♡ \n **﹒enjoy your stay + boost us :)**', color = discord.Color.from_rgb(47, 49, 54))
+        embed = discord.Embed(title=f'', description = f'welcome to {member.guild.name} {member.mention} \n ˚ ༘♡ ⋆｡ [roles](https://discord.gg/mUcFFgQv2X)﹒[ping](https://discord.gg/xWADmpcGMQ)﹒[chat](https://discord.gg/PH7zb2QPUK)﹒˚ ❀ \n **﹒enjoy your stay + boost us :)**', color = discord.Color.from_rgb(47, 49, 54))
         embed.set_footer(text=".gg/rvelvet・enjoy!")
         await client.get_channel(1059592716238991438).send(f"{member.mention}")
         await client.get_channel(1059592716238991438).send(embed=embed)
@@ -27,5 +26,7 @@ async def on_member_join(member):
         #await member.add_roles(role)
     else:
         return
+
+
 
 client.run(os.environ['TOKEN'])
